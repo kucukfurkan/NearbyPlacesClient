e46f0834f7d8384e22e953cd842123adcafbc7eb
Furkan Küçük <furkannkck@outlook.com>
2024-01-08 19:51:13 +0300

    Make the page responsive

--- src/components/NearbyPlaces.vue
+++ src/components/NearbyPlaces.vue
@@ -1,6 +1,6 @@
 <!-- src/components/NearbyPlaces.vue -->
 <template>
-  <div>
+  <div class="nearby-places">
     <h1>Nearby Places</h1>
     <form @submit.prevent="getNearbyPlaces" class="form-container">
       <div class="form-group">
@@ -18,10 +18,10 @@
         <input type="text" class="input-group" v-model="radius" required />
       </div>
 
-      <button type="submit" class="button">Get Nearby Places</button>
+      <button type="submit" class="button">Nearby Places</button>
     </form>
-
   </div>
+  <div></div>
 </template>
   
 <script>
@@ -56,23 +56,63 @@ export default {
   
 <style scoped>
 .form-container {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-}
+    min-height: 100vh; 
+    display: flex;
+    flex-direction: row;
+    max-width: 600px; 
+    margin: 0 auto; 
+    padding: 20px;
+    align-items: flex-start;
+  }
 
-.button {
-  background-color: #4caf50;
-  color: white;
-  padding: 10px 20px;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-  font-size: 16px;
-  height: 55px;
-}
+  .nearby-places{
+    padding-top: 0px;
+  }
 
-.button:hover {
-  background-color: #45a049;
-}
+  .form-group {
+    margin-bottom: 15px;
+    align-items: flex-start;
+  }
+
+  .label-group {
+    display: block;
+    margin-bottom: 5px;
+  }
+
+  .input-group {
+    width: 100%;
+    padding: 8px;
+    box-sizing: border-box;
+    margin-right:24px;
+  }
+
+  .button {
+    background-color: #4caf50;
+    color: white;
+    padding: 10px;
+    border: none;
+    cursor: pointer;
+    width:70px;
+    height: 50px;
+    margin-top: 20px;
+  }
+
+  @media (min-width: 600px) {
+    .form-container {
+      max-width: 600px; 
+    }
+
+    .form-group {
+      flex-direction: row;
+      align-items: center;
+    }
+
+    .label-group {
+      margin-right: 10px;
+    }
+
+    .input-group {
+      width: 150px; 
+    }
+  }
 </style>
